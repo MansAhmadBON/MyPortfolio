2d54cc2caf367ea5a27354df5e1bfb2be9c90d27
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-06-18 14:58:13 +0300

    created portfolio page

---
+++ js/index.js
@@ -0,0 +1,179 @@
+const translations = {
+    "en": {
+        "Language": "язык",
+        "RU": "РУС",
+        "EN": "АНГЛ",
+        "Theme": "Тема",
+        "Light": "Светлая",
+        "Dark": "Тёмная",
+        "Close": "Закрыть",
+        "mansur": "Мансур",
+        "ahmad": "Ахмад",
+        "JavaScript & Front-End Developer": "ДжаваСкрипт и Фронт-енд разработчик",
+        "DOWNLOAD CV": "Скачать резюме",
+        "Location": "Местонахождение",
+        "Minsk, Belarus": "Минск, Беларусь",
+        "Phone": "Телефон",
+        "LinkedIn": "Линкедин",
+        "Email": "Почта",
+        "INTRO": "вступление",
+        "What I am all about.": "О чем это я...",
+        "My name is Mansur. I am very passionate about creating application interfaces. I constantly practice and improve my skills to keep up with the highest standarts. I want my job to contribute to my professional growth.": "Меня зовут Мансур. Я очень увлечен созданием интерфейсов приложений, для этого я постоянно практикуюсь и совершенствую свои навыки. Я очень люблю Front-End и хочу, чтобы работа способствовала моему профессиональному росту.",
+        "Facts About Me": "Факты обо мне",
+        "Focused on results": "Ориентирован на результат",
+        "Responsible": "Ответственный",
+        "Meet deadlines and make every effort to achieve the goal": "Соблюдаю сроки, и прилогаю все силы, для достижения цели",
+        "Able to objectively evaluate capabilities for solving assigned tasks": "Способен объективно оценить возможности для решения поставленных задач",
+        "Ready to work in a team": "Готов к работе в команде",
+        "Ready to continuously learn new technologies and tools": "Готов постоянно изучать новые технологии и инструменты",
+        "Skills": "Навыки",
+        "This is just the beginning!": "Это только начало!",
+        "EXPERIENCE": "Опыт",
+        "Happy time": "Счастливое время",
+        "09.2019 - Present": "09.2019 - по настоящий момент",
+        "PET PROJECTS DEVELOPMENT": "Разработка собственных проектов",
+        "I completed more than 15 projects from page marking to creating the front-end of the SPA. Mastered the main stack front-end technologies.": "Я выполнил более 15 проектов от разметки страницы до создания внешнего интерфейса SPA. Освоил основной стек фронтенд-технологий.",
+        "EDUCATION": "Образование",
+        "Lazy isn't in my vocabulary.": "Лени нет в моем словаре.",
+        "Front-End Development Trainee": "Студент",
+        "IT School TeachMeSkills": "ИТ-школа TeachMeSkills",
+        "I successfully completed two course projects using HTML5, CSS3, JS and a graduation project using React and Redux.": "Я успешно окончил курсы, выполнив две курсовые работы с использованием HTML, CSS, JS и один дипломный проект на React redux",
+        "Biochemist": "Биохимик",
+        "Belarusian State University": "Белорусский государственный университет",
+        "I graduated from the Faculty of Biology of Belarusian State University; Degree in Biochemistry.": "Я окончил биологический факультет Белорусского государственного университета; Степень в области биохимии.",
+        "PORTFOLIO": "Потрфолио",
+        "Here it gets interesting.": "Здесь становится интересно",
+        "HTML, CSS, JS, (latest project)": "HTML, CSS, JS, (Последний проект)",
+        "React, Redux, receiving data from API": "React, Redux, работа с API",
+        "OUTRO": "Заключение",
+        "Call me, maybe.": "Позвони мне, может быть.",
+        "In addition to working on projects, I practice in solving algorithmic problems on the codewars platform. I attend conferences, read articles on specialized resources to keep abreast of the latest development trends.": "Помимо работы над проектами, я занимаюсь решением алгоритмических задач на платформе codewars. Я посещаю конференции, читаю статьи на профильных ресурсах, чтобы быть в курсе последних тенденций разработки",
+        "My primary goal is to create something beautiful, convenient and functional interfaces, setting myself more complex tasks with people that bring out the best in me.": "Моя основная цель - создать что-то красивое, удобное и функциональное. Развиваться, решать более сложные задачи с людьми, которые развивают лучшее во мне."
+    },
+    "ru": {
+        "язык": "Language",
+        "РУС": "RU",
+        "АНГЛ": "EN",
+        "Тема": "Theme",
+        "Светлая": "Light",
+        "Тёмная": "Dark",
+        "Закрыть": "Close",
+        "Мансур": "mansur",
+        "Ахмад": "ahmad",
+        "ДжаваСкрипт и Фронт-енд разработчик": "JavaScript & Front-End Developer",
+        "Скачать резюме": "DOWNLOAD CV",
+        "Местонахождение": "Location",
+        "Минск, Беларусь": "Minsk, Belarus",
+        "Телефон": "Phone",
+        "Линкедин": "LinkedIn",
+        "Почта": "Email",
+        "вступление": "INTRO",
+        "О чем это я...": "What I am all about.",
+        "Меня зовут Мансур. Я очень увлечен созданием интерфейсов приложений, для этого я постоянно практикуюсь и совершенствую свои навыки. Я очень люблю Front-End и хочу, чтобы работа способствовала моему профессиональному росту.": "My name is Mansur. I am very passionate about creating application interfaces. I constantly practice and improve my skills to keep up with the highest standarts. I want my job to contribute to my professional growth.",
+        "Факты обо мне": "Facts About Me",
+        "Ориентирован на результат": "Focused on results",
+        "Ответственный": "Responsible",
+        "Соблюдаю сроки, и прилогаю все силы, для достижения цели": "Meet deadlines and make every effort to achieve the goal",
+        "Способен объективно оценить возможности для решения поставленных задач": "Able to objectively evaluate capabilities for solving assigned tasks",
+        "Готов к работе в команде": "Ready to work in a team",
+        "Готов постоянно изучать новые технологии и инструменты": "Ready to continuously learn new technologies and tools",
+        "Навыки": "Skills",
+        "Это только начало!": "This is just the beginning!",
+        "Опыт": "EXPERIENCE",
+        "Счастливое время": "Happy time",
+        "09.2019 - по настоящий момент": "09.2019 - Present",
+        "Разработка собственных проектов": "PET PROJECTS DEVELOPMENT",
+        "Я выполнил более 15 проектов от разметки страницы до создания внешнего интерфейса SPA. Освоил основной стек фронтенд-технологий.": "I completed more than 15 projects from page marking to creating the front-end of the SPA. Mastered the main stack front-end technologies.",
+        "Образование": "EDUCATION",
+        "Лени нет в моем словаре.": "Lazy isn't in my vocabulary.",
+        "Студент": "Front-End Development Trainee",
+        "ИТ-школа TeachMeSkills": "IT School TeachMeSkills",
+        "Я успешно окончил курсы, выполнив две курсовые работы с использованием HTML, CSS, JS и один дипломный проект на React redux": "I completed more than 15 projects from page marking to creating the front-end of the SPA. Mastered the main stack front-end technologies",
+        "Биохимик": "Biochemist",
+        "Белорусский государственный университет": "Belarusian State University",
+        "Я окончил биологический факультет Белорусского государственного университета; Степень в области биохимии.": "I graduated from the Faculty of Biology of Belarusian State University; Degree in Biochemistry.",
+        "Потрфолио": "PORTFOLIO",
+        "Здесь становится интересно": "Here it gets interesting.",
+        "HTML, CSS, JS, (Последний проект)": "HTML, CSS, JS, (latest project)",
+        "React, Redux, работа с API": "React, Redux, receiving data from API",
+        "Заключение": "OUTRO",
+        "Позвони мне, может быть.": "Call me, maybe.",
+        "Помимо работы над проектами, я занимаюсь решением алгоритмических задач на платформе codewars. Я посещаю конференции, читаю статьи на профильных ресурсах, чтобы быть в курсе последних тенденций разработки": "In addition to working on projects, I practice in solving algorithmic problems on the codewars platform. I attend conferences, read articles on specialized resources to keep abreast of the latest development trends.",
+        "Моя основная цель - создать что-то красивое, удобное и функциональное. Развиваться, решать более сложные задачи с людьми, которые развивают лучшее во мне.": "My primary goal is to create something beautiful, convenient and functional interfaces, setting myself more complex tasks with people that bring out the best in me."
+    }
+}
+window.onload = function () {
+    const $setting = document.querySelector('.settings');
+    const $settingsCtrl = document.querySelector('.settings-ctrl');
+    const $sectionsHeaderOdd = document.querySelectorAll('.section-header--odd');
+    const $sectionsContentOdd = document.querySelectorAll('.section-content--odd');
+    const $mainBox = document.querySelector('.main-box');
+    const $sectionsHeaderEven = document.querySelectorAll('.section-header--even');
+    const $sectionsContentEven = document.querySelectorAll('.section-content--even');
+
+    $settingsCtrl.addEventListener('click', () => {
+        $setting.classList.add('settings--active');
+        $settingsCtrl.classList.add('settings-ctrl--hide');
+    });
+
+    document.querySelector('.close').addEventListener('click', () => {
+        $setting.classList.remove('settings--active');
+        $settingsCtrl.classList.remove('settings-ctrl--hide');
+    });
+
+
+    //DARK THEME
+    function changeThemePage(theme, sectionsHeaderOdd, sectionsContentOdd, mainBox, setting, sectionsHeaderEven, sectionsContentEven){
+        if(theme === 'dark'){
+            sectionsHeaderOdd.forEach(sectionHeader => sectionHeader.classList.add('dark-theme--section-header--odd'));
+            sectionsContentOdd.forEach(sectionContent => sectionContent.classList.add('dark-theme--section-content--odd'));
+            mainBox.classList.add('dark-theme--section-content--odd');
+            setting.classList.add('dark-theme--section-content--odd');
+            sectionsHeaderEven.forEach(sectionHeader => sectionHeader.classList.add('dark-theme--section-header--even'));
+            sectionsContentEven.forEach(sectionContent => sectionContent.classList.add('dark-theme--section-content--even'));
+        }
+        if(theme === 'light'){
+            sectionsHeaderOdd.forEach(sectionHeader => sectionHeader.classList.remove('dark-theme--section-header--odd'));
+            sectionsContentOdd.forEach(sectionContent => sectionContent.classList.remove('dark-theme--section-content--odd'));
+            mainBox.classList.remove('dark-theme--section-content--odd');
+            setting.classList.remove('dark-theme--section-content--odd');
+            sectionsHeaderEven.forEach(sectionHeader => sectionHeader.classList.remove('dark-theme--section-header--even'));
+            sectionsContentEven.forEach(sectionContent => sectionContent.classList.remove('dark-theme--section-content--even'));
+        }
+
+    }
+    document.querySelector('.theme__variant--dark').addEventListener('click', () => {
+        changeThemePage('dark', $sectionsHeaderOdd, $sectionsContentOdd, $mainBox, $setting, $sectionsHeaderEven, $sectionsContentEven)
+    });
+
+    //LIGHT THEME
+    document.querySelector('.theme__variant--light').addEventListener('click', () => {
+        changeThemePage('light', $sectionsHeaderOdd, $sectionsContentOdd, $mainBox, $setting, $sectionsHeaderEven, $sectionsContentEven)
+    });
+
+
+    //Multi Language
+    const $textElements = document.querySelectorAll(`[data-i18n]`);
+    const btnRu = document.querySelector('.language__variant--ru');
+    const btnEn = document.querySelector('.language__variant--en');
+    let  currentLang = 'en';
+
+    btnRu.addEventListener("click", function () {
+        if(currentLang !== 'ru'){
+            $textElements.forEach(element => {
+                element.textContent = translations.en[element.textContent.trim()]
+            });
+            currentLang = 'ru';
+        }
+    });
+
+    btnEn.addEventListener("click", function () {
+        if(currentLang !== 'en'){
+            $textElements.forEach(element => {
+                element.textContent = translations.ru[element.textContent.trim()]
+            });
+        }
+        currentLang = 'en';
+    });
+    
+};
